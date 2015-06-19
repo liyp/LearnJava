501befaacc3b790cad6b413ce84f2a5fb17f7b61
Peng:-) <liyp.yunpeng@gmail.com>
2015-06-19 09:05:35 +0800

    init proj-mina

---
+++ proj-mina/src/main/java/com/github/liyp/mina/buffer/BufferFillDrain.java
@@ -0,0 +1,41 @@
+package com.github.liyp.mina.buffer;
+
+import java.nio.CharBuffer;
+
+/**
+ * http://my.oschina.net/ielts0909/blog/84876
+ */
+public class BufferFillDrain {
+    private static int index = 0;
+    private static String[] strings = { "A random string value",
+            "The product of an infinite number of monkeys",
+            "Hey hey we're the Monkees" };
+
+    private static boolean fillBuffer(CharBuffer buffer) {
+        if (index >= strings.length) {
+            return false;
+        }
+        String string = strings[index++];
+
+        for (int i = 0; i < string.length(); i++) {
+            buffer.put(string.charAt(i));
+        }
+        return true;
+    }
+
+    private static void drainBuffer(CharBuffer buffer) {
+        while (buffer.hasRemaining()) {
+            System.out.println(buffer.get());
+        }
+        System.err.println("");
+    }
+
+    public static void main(String[] args) {
+        CharBuffer buffer = CharBuffer.allocate(100);
+        while (fillBuffer(buffer)) {
+            buffer.flip();
+            drainBuffer(buffer);
+            buffer.clear();
+        }
+    }
+}
